f0b91d4551ad228e51822bd222bb7f0029515a5b
RobNor12 <rnorris12021@gmail.com>
2025-10-28 07:37:45 -0500

    Update volume.c

--- Week - 04/Volume/volume.c
+++ Week - 04/Volume/volume.c
@@ -11,23 +11,23 @@ int main(int argc, char *argv[]){
 
     // Check command-line arguments
     if (argc != 4){
-
         printf("Usage: ./volume input.wav output.wav factor\n");
+        
         return 1;
     }
 
     // Open files and determine scaling factor
     FILE *input = fopen(argv[1], "r");
     if (input == NULL){
-
         printf("Could not open file.\n");
+        
         return 1;
     }
 
     FILE *output = fopen(argv[2], "w");
     if (output == NULL){
-
         printf("Could not open file.\n");
+        
         return 1;
     }
 
